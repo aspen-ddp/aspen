b67e71b9c2f67ceeedf453d8425d026c5ce7322b
Tom Cocagne <tom.cocagne@gmail.com>
2026-05-17 14:19:47 -0500

    Refactor uuid/value encoding/decoding

--- src/main/scala/org/aspen_ddp/aspen/client/registries/NamespacedUUIDRegistry.scala
+++ src/main/scala/org/aspen_ddp/aspen/client/registries/NamespacedUUIDRegistry.scala
@@ -2,34 +2,25 @@ package org.aspen_ddp.aspen.client.registries
 
 import org.aspen_ddp.aspen.client.{AspenClient, Transaction}
 import org.aspen_ddp.aspen.common.objects.{Key, KeyValueObjectPointer, Value}
+import org.aspen_ddp.aspen.common.util.{byte2uuid, uuid2byte}
 
-import java.nio.ByteBuffer
 import java.util.UUID
 import scala.concurrent.{ExecutionContext, Future}
 
 object NamespacedUUIDRegistry:
   def makeKey(namespace: String, name: String): Key = Key(s"$namespace.$name")
+  def encodeUUID(uuid: UUID): Value = Value(uuid2byte(uuid))
+  def decodeUUID(value: Value): UUID = byte2uuid(value.bytes)
 
 class NamespacedUUIDRegistry(val client: AspenClient,
                              containingObjectPtr: KeyValueObjectPointer,
                              treeRootKey: Key):
 
-  import NamespacedUUIDRegistry.makeKey
+  import NamespacedUUIDRegistry.{makeKey, encodeUUID, decodeUUID}
 
   given ExecutionContext = client.clientContext
 
   private val registry = Registry(client, containingObjectPtr, treeRootKey)
-
-  private def encodeUUID(uuid: UUID): Value =
-    val arr = new Array[Byte](16)
-    val bb = ByteBuffer.wrap(arr)
-    bb.putLong(uuid.getMostSignificantBits)
-    bb.putLong(uuid.getLeastSignificantBits)
-    Value(arr)
-
-  private def decodeUUID(value: Value): UUID =
-    val bb = ByteBuffer.wrap(value.bytes)
-    new UUID(bb.getLong(), bb.getLong())
 
   def getRegisteredObject(namespace: String, name: String): Future[UUID] =
     registry.get(makeKey(namespace, name)).map(decodeUUID)

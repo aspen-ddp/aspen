94d97d2b2901fa823e30b04e30d61408f3fc0434
Tom Cocagne <tom.cocagne@gmail.com>
2026-05-17 18:44:08 -0500

    Remove StorePointer type and simplify Locater to ObjectId alias

--- src/main/scala/org/aspen_ddp/aspen/server/store/Locater.scala
+++ src/main/scala/org/aspen_ddp/aspen/server/store/Locater.scala
@@ -1,6 +1,5 @@
 package org.aspen_ddp.aspen.server.store
 
 import org.aspen_ddp.aspen.common.objects.ObjectId
-import org.aspen_ddp.aspen.common.store.StorePointer
 
-case class Locater(objectId: ObjectId, storePointer: StorePointer)
+type Locater = ObjectId

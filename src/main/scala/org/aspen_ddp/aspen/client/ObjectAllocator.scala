251da99b9aab6e5d542806bc7efc918c8b889014
Tom Cocagne <tom.cocagne@gmail.com>
2026-05-19 20:01:59 -0500

    Move allocate-with-content implementation to ObjectAllocator

--- src/main/scala/org/aspen_ddp/aspen/client/ObjectAllocator.scala
+++ src/main/scala/org/aspen_ddp/aspen/client/ObjectAllocator.scala
@@ -2,9 +2,9 @@ package org.aspen_ddp.aspen.client
 
 import org.aspen_ddp.aspen.common.DataBuffer
 import org.aspen_ddp.aspen.common.ida.IDA
-import org.aspen_ddp.aspen.common.objects.{AllocationRevisionGuard, DataObjectPointer, Key, KeyValueObjectPointer, KeyValueOperation, ObjectRefcount, Value}
+import org.aspen_ddp.aspen.common.objects.{AllocationRevisionGuard, DataObjectPointer, Insert, Key, KeyValueObjectPointer, KeyValueOperation, ObjectRefcount, ObjectRevision, SetLeft, SetMax, SetMin, SetRight, Value}
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait ObjectAllocator:
 
@@ -12,13 +12,20 @@ trait ObjectAllocator:
 
   val objectIDA: IDA
 
+  protected val executionContext: ExecutionContext
+
   def allocateDataObject()(using t: Transaction): Future[DataObjectPointer]
 
   def allocateKeyValueObject()(using t: Transaction): Future[KeyValueObjectPointer]
 
   def allocateDataObject(revisionGuard: AllocationRevisionGuard,
                          initialContent: DataBuffer,
-                         initialRefcount: ObjectRefcount = ObjectRefcount(0,1))(using t: Transaction): Future[DataObjectPointer]
+                         initialRefcount: ObjectRefcount = ObjectRefcount(0,1))(using t: Transaction): Future[DataObjectPointer] =
+    given ExecutionContext = executionContext
+    allocateDataObject().map: ptr =>
+      t.overwrite(ptr, ObjectRevision.Allocating, initialContent)
+      t.setRefcount(ptr, ObjectRefcount.Allocating, initialRefcount)
+      ptr
 
   def allocateKeyValueObject(revisionGuard: AllocationRevisionGuard,
                              initialContent: Map[Key,Value],
@@ -26,5 +33,23 @@ trait ObjectAllocator:
                              maximum: Option[Key] = None,
                              left: Option[Value] = None,
                              right: Option[Value] = None,
-                             initialRefcount: ObjectRefcount = ObjectRefcount(0,1))(using t: Transaction): Future[KeyValueObjectPointer]
+                             initialRefcount: ObjectRefcount = ObjectRefcount(0,1))(using t: Transaction): Future[KeyValueObjectPointer] =
+    given ExecutionContext = executionContext
+    allocateKeyValueObject().map: ptr =>
+
+      var ops: List[KeyValueOperation] = initialContent.map((k, v) => Insert(k, v.bytes)).toList
+
+      minimum.foreach: min =>
+        ops = SetMin(min) :: ops
+      maximum.foreach: max =>
+        ops = SetMax(max) :: ops
+      left.foreach: left =>
+        ops = SetLeft(left.bytes) :: ops
+      right.foreach: right =>
+        ops = SetRight(right.bytes) :: ops
+
+      t.update(ptr, None, None, Nil, ops)
+      t.setRefcount(ptr, ObjectRefcount.Allocating, initialRefcount)
+
+      ptr
 
--- src/main/scala/org/aspen_ddp/aspen/client/internal/allocation/SinglePoolObjectAllocator.scala
+++ src/main/scala/org/aspen_ddp/aspen/client/internal/allocation/SinglePoolObjectAllocator.scala
@@ -14,8 +14,8 @@ class SinglePoolObjectAllocator(val client: AspenClient,
                                 val objectIDA: IDA,
                                 val maxObjectSize: Option[Int]) extends ObjectAllocator:
 
-  given ExecutionContext = client.clientContext
-
+  val executionContext: ExecutionContext = client.clientContext
+  
   override def allocateDataObject()(using t: Transaction): Future[DataObjectPointer] =
     val ptr = new DataObjectPointer(ObjectId(UUID.randomUUID()), pool.poolId, Array.empty)
     t.addAllocatingObject(ptr)
@@ -28,36 +28,5 @@ class SinglePoolObjectAllocator(val client: AspenClient,
     AllocationFinalizationAction.addToTransaction(ptr, t)
     Future.successful(ptr)
 
-  override def allocateDataObject(revisionGuard: AllocationRevisionGuard,
-                                  initialContent: DataBuffer,
-                                  initialRefcount: ObjectRefcount)(using t: Transaction): Future[DataObjectPointer] =
-    allocateDataObject().map: ptr =>
-      t.overwrite(ptr, ObjectRevision.Allocating, initialContent)
-      t.setRefcount(ptr, ObjectRefcount.Allocating, initialRefcount)
-      ptr
-
-  override def allocateKeyValueObject(revisionGuard: AllocationRevisionGuard,
-                                      initialContent: Map[Key, Value],
-                                      minimum: Option[Key],
-                                      maximum: Option[Key],
-                                      left: Option[Value],
-                                      right: Option[Value],
-                                      initialRefcount: ObjectRefcount)(using t: Transaction): Future[KeyValueObjectPointer] =
-    allocateKeyValueObject().map: ptr =>
-
-      var ops: List[KeyValueOperation] = initialContent.map((k,v) => Insert(k, v.bytes)).toList
-
-      minimum.foreach: min =>
-        ops = SetMin(min) :: ops
-      maximum.foreach: max =>
-        ops = SetMax(max) :: ops
-      left.foreach: left =>
-        ops = SetLeft(left.bytes) :: ops
-      right.foreach: right =>
-        ops = SetRight(right.bytes) :: ops
-
-      t.update(ptr, None, None, Nil, ops)
-      t.setRefcount(ptr, ObjectRefcount.Allocating, initialRefcount)
-
-      ptr
+  
 

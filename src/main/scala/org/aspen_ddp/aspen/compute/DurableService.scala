aef3492820dd44193c6ca3ee2d79299deeaba77f
Tom Cocagne <tom.cocagne@gmail.com>
2026-07-03 17:48:25 -0500

    Make DurableService.receiveMessage a default no-op

--- src/main/scala/org/aspen_ddp/aspen/compute/DurableService.scala
+++ src/main/scala/org/aspen_ddp/aspen/compute/DurableService.scala
@@ -1,6 +1,12 @@
 package org.aspen_ddp.aspen.compute
 
+import org.aspen_ddp.aspen.common.network.ServiceMessage
+
 trait DurableService:
   /** Called by the executor when this host loses its lease.
    *  The service must stop operating immediately. */
   def shutdown(): Unit
+
+  /** Called by the executor when a ServiceMessage arrives for this service.
+   *  Default is a no-op; services that consume messages override this. */
+  def receiveMessage(msg: ServiceMessage): Unit = ()
